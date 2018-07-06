0cbbb67a243518b1131543c763c5a1813b8731e8
shizibaitou <wj06_job@163.com>
2018-07-06 18:12:11 +0800

    add

--- src/components/List.vue
+++ src/components/List.vue
@@ -23,12 +23,20 @@ export default {
     background-image: -webkit-linear-gradient(left,blue,#66ffff 10%,#cc00ff 20%,#CC00CC 30%, #CCCCFF 40%, #00FFFF 50%,#CCCCFF 60%,#CC00CC 70%,#CC00FF 80%,#66FFFF 90%,blue 100%);
     background-size: contain;
     background-clip:content-box ;
-    animation: 0s;
+    animation: masked-animation 4s linear infinite;
     -webkit-text-fill-color: transparent;
     -webkit-background-clip: text;
     -webkit-background-size: 200% 100%; 
     -webkit-animation: masked-animation 4s linear infinite;
     font-size: 35px;
+}
+@keyframes masked-animation {
+    0% {
+        background-position: 0  0;
+    }
+    100% {
+        background-position: -100%  0;
+    }
 }
 </style>
 
